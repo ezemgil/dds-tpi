ec7be584a898de49d08e3e0ec0d2e03b0e9b74e0
agusgut <89768@sistemas.frc.utn.edu.ar>
2024-05-29 01:34:05 -0300

    CRUD Idiomas

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import errorHandler from "./middleware/errorHandler.js";
 
 // Importar rutas
 import generoRoutes from "./routes/genero.routes.js";
+import idiomaRoutes from "./routes/idioma.routes.js"
 
 // Crear aplicación express
 const app = express();
@@ -17,6 +18,7 @@ app.use(errorHandler);
 
 // Rutas
 app.use(generoRoutes);
+app.use(idiomaRoutes);
 
 // Iniciar el servidor
 sequelize.sync().then(() => {
---
+++ backend/controllers/idiomaController.js
@@ -0,0 +1,64 @@
+import * as idiomaService from "../services/idiomaService.js"
+import { NotFoundError, BadRequestError } from "../utils/errors.js"
+
+// Buscar todos los idiomas
+export const getIdiomas = async (req, res, next) => {
+    try {
+        const idiomas = await idiomaService.getIdiomas();
+        res.json(idiomas);
+    } catch(error) {
+        next(error);
+    }
+};
+
+// Buscar idioma por id
+export const getIdiomaById = async (req, res, next) => {
+    try {
+        const idioma = await idiomaService.getIdiomaById(req.params.id);
+        if (idioma) {
+            res.json(idioma);
+        } else {
+            next(new NotFoundError("Idiomas no encontrado"));
+        }
+    } catch(error) {
+        next(error);
+    }
+};
+
+// Crear idioma
+export const createIdioma = async (req, res, next) => {
+    try {
+        const idioma = await idiomaService.createIdioma(req.body);
+        res.status(201).json(idioma)
+    } catch(error) {
+        next(error);
+    }
+};
+
+// Actualizar idioma
+export const updateIdioma = async (req, res, next) => {
+    try {
+        const idioma = await idiomaService.updateIdioma(req.params.id, req.body);
+        if (idioma) {
+            res.json(idioma)
+        } else {
+            next(new NotFoundError("Idioma no encontrado"));
+        }
+    } catch(error) {
+        next(error);
+    }
+};
+
+// Eliminar idioma
+export const deleteIdioma = async (req, res, next) => {
+    try {
+        const result = await idiomaService.deleteIdioma(req.params.id);
+        if (result) {
+            res.send("Idioma eliminado");
+        } else {
+            next(new NotFoundError("Idioma no encontrado"));
+        }
+    } catch(error) {
+        next(error);
+    }
+};
