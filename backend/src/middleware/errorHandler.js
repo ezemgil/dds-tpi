be24d973674cbca9d6a8acbe54822d91256b14b4
Matías Ezequiel Gil <89765@sistemas.frc.utn.edu.ar>
2024-06-26 16:14:24 -0300

    errorHandler responde como json

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,13 +1,17 @@
-// Error Handler
-const errorHandler = (err, req, res, next) => {
+function errorHandler(err, req, res, next) {
+  // Determina el código de estado HTTP del error, si no existe, utiliza 500
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
 
+  // Log del error para el seguimiento interno
+  console.error(err);
+
+  // Responde al cliente con el error en formato JSON
   res.status(statusCode).json({
     status: "error",
     statusCode,
-    message: message,
+    message: err.message,
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
-};
+}
 
 export default errorHandler;
