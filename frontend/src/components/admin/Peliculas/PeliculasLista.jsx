10130392faf90d41f8337d95cee9d0d3c0014ffa
witt97848 <97848@sistemas.frc.utn.edu.ar>
2024-06-26 17:53:13 -0300

    Fix 'PeliculasLista.jsx' para hacer commit rama Frontend

--- frontend/src/components/admin/Peliculas/PeliculasLista.jsx
+++ frontend/src/components/admin/Peliculas/PeliculasLista.jsx
@@ -23,34 +23,34 @@ const PeliculasLista = ({ Peliculas, Editar, Eliminar }) => {
                     {Peliculas.length > 0 ? (
                         Peliculas?.map((Pelicula) => (
                             <tr key={Pelicula.id}>
-                            <td>{Pelicula.id}</td>
-                            <td>{Pelicula.titulo}</td>
-                            <td>{Pelicula.calificacion}</td>
-                            <td>{Pelicula.duracion}</td>
-                            <td>{Pelicula.fecha_estreno}</td>
-                            
-                            <td className="d-flex gap-2 justify-content-center">
-                                <Link to={`/pelicula/${Pelicula.id}`}>
-                                    <button className="btn btn-info btn-sm rounded-pill">
-                                        <i className="fa-solid fa-eye text-primary-emphasis"></i>
+                                <td>{Pelicula.id}</td>
+                                <td>{Pelicula.titulo}</td>
+                                <td>{Pelicula.calificacion}</td>
+                                <td>{Pelicula.duracion}</td>
+                                <td>{Pelicula.fecha_estreno}</td>
+                                
+                                <td className="d-flex gap-2 justify-content-center">
+                                    <Link to={`/pelicula/${Pelicula.id}`}>
+                                        <button className="btn btn-info btn-sm rounded-pill">
+                                            <i className="fa-solid fa-eye text-primary-emphasis"></i>
+                                        </button>
+                                    </Link>
+                                    <button
+                                    className="btn btn-warning btn-sm rounded-pill"
+                                    onClick={() => Editar(Pelicula.id)}
+                                    >
+                                    <i className="fa-solid fa-pencil text-danger-emphasis"></i>
                                     </button>
-                                </Link>
-                                <button
-                                className="btn btn-warning btn-sm rounded-pill"
-                                onClick={() => Editar(Pelicula.id)}
-                                >
-                                <i className="fa-solid fa-pencil text-danger-emphasis"></i>
-                                </button>
-                                <button
-                                className="btn btn-danger btn-sm rounded-pill"
-                                onClick={() => Eliminar(Pelicula.id)}
-                                >
-                                <i className="fa-solid fa-trash-can text-danger-emphasis"></i>
-                                </button>
-                            </td>
+                                    <button
+                                    className="btn btn-danger btn-sm rounded-pill"
+                                    onClick={() => Eliminar(Pelicula.id)}
+                                    >
+                                    <i className="fa-solid fa-trash-can text-danger-emphasis"></i>
+                                    </button>
+                                </td>
                             </tr>
                         ))
-                    ) 
+                    )
                     : (<tr><td colSpan="6">No hay peliculas</td></tr>)}
                     </tbody>
                 </table>
