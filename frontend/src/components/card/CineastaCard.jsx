745b5e28aa4e77cad9c5aee2842d8a6bae00a59b
Matías Ezequiel Gil <89765@sistemas.frc.utn.edu.ar>
2024-07-01 15:32:34 -0300

    Manejo con tabs

--- frontend/src/components/card/CineastaCard.jsx
+++ frontend/src/components/card/CineastaCard.jsx
@@ -3,51 +3,44 @@ import moment from "moment";
 import { useNavigate } from "react-router-dom";
 
 const CineastaCard = ({ Id, Nombre, FechaNacimiento, FechaFallecimiento, Roles, Imagen }) => {
-  let navigate = useNavigate();
-  const handleClick = () => {
-    navigate(`/cineasta/${Id}`);
-  };
-  return (
-    <div className="col mb-4" onClick={handleClick}>
-      <div className="card h-100 bg-dark text-white">
-        <img
-          src={Imagen}
-          className="bd-placeholder-img card-img-top"
-          width="100%"
-          height="300"
-          alt={Nombre}
-        />
-        <div className="card-body">
-          <div className="d-flex justify-content-between align-items-center mb-2">
-            <h5 className="card-title mb-0">{Nombre}</h5>
-            {FechaFallecimiento && (
-              <span className="text-warning">
-                {FechaFallecimiento
-                  ? `${moment(FechaFallecimiento).diff(FechaNacimiento, "years")} años (fallecido)`:""}
-              </span>
-            )}
-            {FechaNacimiento && !FechaFallecimiento && (
-              <span className="text-warning">
-                {moment().diff(FechaNacimiento, "years")} años
-              </span>
-            )}
-          </div>
-          <div className="d-flex gap-1 flex-wrap">
-            {Roles &&
-              Roles?.length > 0 &&
-              Roles.map((rol) => (
-                <span
-                  key={rol.id}
-                  className="badge bg-danger-subtle border border-danger-subtle text-danger-emphasis rounded-pill"
-                >
-                  {rol.nombre}
-                </span>
-              ))}
-          </div>
+    let navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`/cineasta/${Id}`);
+    };
+    return (
+        <div className="col mb-4" onClick={handleClick}>
+            <div className="card h-100 bg-dark text-white" focusable="true" tabIndex="0" role="button" 
+                <img src={Imagen} className="bd-placeholder-img card-img-top" width="100%" height="300" alt={Nombre} />
+                <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center mb-2">
+                        <h5 className="card-title mb-0">{Nombre}</h5>
+                        {FechaFallecimiento && (
+                            <span className="text-warning">
+                                {FechaFallecimiento
+                                    ? `${moment(FechaFallecimiento).diff(FechaNacimiento, "years")} años (fallecido)`
+                                    : ""}
+                            </span>
+                        )}
+                        {FechaNacimiento && !FechaFallecimiento && (
+                            <span className="text-warning">{moment().diff(FechaNacimiento, "years")} años</span>
+                        )}
+                    </div>
+                    <div className="d-flex gap-1 flex-wrap">
+                        {Roles &&
+                            Roles?.length > 0 &&
+                            Roles.map((rol) => (
+                                <span
+                                    key={rol.id}
+                                    className="badge bg-danger-subtle border border-danger-subtle text-danger-emphasis rounded-pill"
+                                >
+                                    {rol.nombre}
+                                </span>
+                            ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CineastaCard;
--- frontend/src/components/card/NominacionCard.jsx
+++ frontend/src/components/card/NominacionCard.jsx
@@ -2,36 +2,40 @@ import moment from "moment";
 import React from "react";
 
 const NominacionCard = ({ Nominacion }) => {
-  return (
-    <div className="d-flex align-items-center justify-content-start gap-3">
-      <div className="d-flex justify-content-start align-items-center gap-3">
-        <i
-          class={
-            "fa-solid fa-award fs-1 " +
-            (Nominacion.fue_ganador === 0 ? "text-secondary" : "text-warning")
-          }
-        ></i>
-        <div>
-          <div className="d-flex gap-2 align-items-center">
-            <span className="fs-6">
-              <b>Año: </b>
-              {moment(Nominacion.fecha_nominacion).format("YYYY")}
-            </span>
-            {Nominacion.fue_ganador ? (
-              <span className="badge bg-warning-subtle border border-warning-subtle text-warning-emphasis rounded-pill    ">
-                Ganador
-              </span>
-            ) : (
-              <span className="badge bg-dark-subtle border border-dark-subtle text-dark-emphasis rounded-pill">
-                Nominado
-              </span>
-            )}
-          </div>
-          <h4 className="fs-4 card-title">{Nominacion.premio.nombre}</h4>
+    return (
+        <div
+            className="d-flex align-items-center justify-content-start gap-3"
+            focusable="true"
+            tabIndex="0"
+            role="button"
+        >
+            <div className="d-flex justify-content-start align-items-center gap-3">
+                <i
+                    class={
+                        "fa-solid fa-award fs-1 " + (Nominacion.fue_ganador === 0 ? "text-secondary" : "text-warning")
+                    }
+                ></i>
+                <div>
+                    <div className="d-flex gap-2 align-items-center">
+                        <span className="fs-6">
+                            <b>Año: </b>
+                            {moment(Nominacion.fecha_nominacion).format("YYYY")}
+                        </span>
+                        {Nominacion.fue_ganador ? (
+                            <span className="badge bg-warning-subtle border border-warning-subtle text-warning-emphasis rounded-pill    ">
+                                Ganador
+                            </span>
+                        ) : (
+                            <span className="badge bg-dark-subtle border border-dark-subtle text-dark-emphasis rounded-pill">
+                                Nominado
+                            </span>
+                        )}
+                    </div>
+                    <h4 className="fs-4 card-title">{Nominacion.premio.nombre}</h4>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default NominacionCard;
